d65fb5ee92c7863bfc5d4270003f4ce233fb55bb
charaekeow <charaekeow11@gmail.com>
2023-12-11 23:56:20 +0800

    FIX(migration): Update user table columns

--- database/migrations/20230709-user-table.ts
+++ database/migrations/20230709-user-table.ts
@@ -3,10 +3,15 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable('user', (table) => {
 		table.uuid('id').primary();
-		table.string('username');
-		table.string('email').unique();
-		table.string('password');
+		table.string('username'); // this is actually the name returned by providers for now
+		table.string('email');
+		table.enum('provider', ['google', 'github']);
+		table.text('image_url');
+
+		// table.string('password');
 		table.timestamp('created_at').defaultTo(knex.fn.now());
+
+		table.unique(['email', 'provider']);
 	});
 }
 
